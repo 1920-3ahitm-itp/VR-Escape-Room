fcfd198810d2590c2faafa45b9c5c81688a7e301
Sarah <feichtinger.sarah5@gmail.com>
2020-04-07 14:36:50 +0200

    skripten für kessel erstellt

---
+++ game/escape/Assets/MyScenes/sktipt/Recipe.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Recipe : MonoBehaviour
+{
+    List<GameObject> ingredients = new List<GameObject>();
+    public GameObject ing1;
+    public GameObject ing2;
+    public GameObject ing3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ing1 = GameObject.Find("/ingredients/Ing1");
+        ing2 = GameObject.Find("/ingredients/Ing2");
+        ing3 = GameObject.Find("/ingredients/Ing3");
+        ingredients.Add(ing1);
+        ingredients.Add(ing2);
+        ingredients.Add(ing3);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
