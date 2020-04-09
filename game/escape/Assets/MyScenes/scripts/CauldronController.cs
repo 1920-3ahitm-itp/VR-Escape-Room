8a0f20fa81aaea2e45ea972c4ab84a2bd1b50ae0
Sarah <feichtinger.sarah5@gmail.com>
2020-04-09 10:33:18 +0200

    couldronController skript update

---
+++ game/escape/Assets/MyScenes/scripts/CauldronController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CauldronController : MonoBehaviour
+{
+    List<GameObject> appendedIngredients = new List<GameObject>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        GameObject other = collision.gameObject;
+
+        // wenn das gameobject der input vom Zauberstab ist, soll das checkObAlleZutatenImKesselSind() skript aufgerufen
+        if (other.CompareTag("Projectile"))
+        {
+            //other.GetComponent<EnemyController>().CallYourFunctionHere();
+
+            compareIngredients();
+            appendedIngredients.Clear();
+        }
+        // alles andere wird als zutat gezählt und zur liste hinzugefügt
+        else
+        {
+            appendedIngredients.Add(other);
+            //Destroy(this.gameObject);
+            Destroy(other);
+        }
+    }
+
+    private void compareIngredients()
+    {
+        //Destroy(this.gameObject);    
+    }
+}
