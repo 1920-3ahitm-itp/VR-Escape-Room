f16ee7ba90883219f50f0f360bbdda56ddf8905a
Sarah <feichtinger.sarah5@gmail.com>
2020-06-14 19:42:08 +0200

    möbel Änderungen

--- game/escape/Assets/MyScenes/scripts/weakGlass.cs
+++ game/escape/Assets/MyScenes/scripts/weakGlass.cs
@@ -15,7 +15,7 @@ public class weakGlass : MonoBehaviour
     {
         //glassIsBreakable = false;
         potionScript = this.GetComponent<Potion>();
-        potionScript.glassIsWeak = false;
+        //potionScript.glassIsWeak = false;
     }
 
     // Update is called once per frame
@@ -24,7 +24,7 @@ public class weakGlass : MonoBehaviour
         
     }
 
-    private void OnCollisionEnter(Collision collision)
+    /*private void OnCollisionEnter(Collision collision)
     {
         GameObject other = collision.gameObject;
 
@@ -33,16 +33,31 @@ public class weakGlass : MonoBehaviour
             potionScript.glassIsWeak = true;
             // Brech funktion aktivieren
         }
+    }*/
+
+
+    private void OnParticleTrigger()
+    {
+        Destroy(this.gameObject);
     }
 
 
-
-    /*void OnParticleCollision(GameObject collision)
+    void OnParticleCollision(GameObject collision)
     {
+        Destroy(this.gameObject);
+        Destroy(collision.gameObject);
+
         if (collision.CompareTag("glassPoison"))
         {
-            Destroy(this.gameObject);
+            //Destroy(this.gameObject);
+            //Destroy(collision.gameObject);
             potionScript.glassIsWeak = true;
         }
-    }*/
+        else if (collision.tag == "glassPoision")
+        {
+            //Destroy(this.gameObject);
+            //Destroy(collision.gameObject);
+            potionScript.glassIsWeak = true;
+        }
+    }
 }
