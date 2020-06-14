c3c9a7bffb24c55f4e0650038daa93b002b2b53c
Dumfarth-Felix <felix012343d@gmail.com>
2020-06-14 21:17:45 +0200

    Added finish UI

---
+++ game/escape/Assets/MyScenes/scripts/finishEvent.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+ using UnityEngine.UI;
+
+public class finishEvent : MonoBehaviour
+{
+    Renderer rend;
+    Text finishText;
+    float time;
+
+    void Start()
+    {
+       
+        gameObject.GetComponent<Text>().enabled = false;
+
+    }
+
+
+    void Update()
+    {
+        time += Time.deltaTime;
+
+    }
+
+    public void finish()
+    {
+        string minutes = Mathf.Floor(time / 60).ToString("00");
+        string seconds = (time % 60).ToString("00");
+        gameObject.GetComponent<Text>().enabled = true;
+       gameObject.GetComponent<Text>().text = "Gratulation\ndeine Zeit:\n"+ string.Format("{0}:{1}", minutes, seconds);
+    }
+
+}
---
+++ game/escape/Assets/MyScenes/scripts/image.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class image : MonoBehaviour
+{
+  
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.GetComponent<Image>().enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void finish()
+    {
+        gameObject.GetComponent<Image>().enabled = true;
+    }
+}
